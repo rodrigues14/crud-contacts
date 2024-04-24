d2e07640dca6408fe321afec5a14a145e7ab33ae
Lucas Rodrigues <lukinhasr1423@gmail.com>
2024-04-24 15:06:35 -0300

    standardized returns of controller

--- src/main/java/br/com/lucasdev/contacts/controller/ContactController.java
+++ src/main/java/br/com/lucasdev/contacts/controller/ContactController.java
@@ -1,13 +1,14 @@
 package br.com.lucasdev.contacts.controller;
 
-import br.com.lucasdev.contacts.dto.AddressContactDTO;
 import br.com.lucasdev.contacts.dto.ContactDTO;
 import br.com.lucasdev.contacts.domain.contact.Contact;
 import br.com.lucasdev.contacts.repository.ContactRepository;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.util.UriComponentsBuilder;
 
 import java.util.List;
 import java.util.Optional;
@@ -21,30 +22,42 @@ public class ContactController {
 
     @PostMapping
     @Transactional
-    public void register(@RequestBody @Valid ContactDTO dados) {
-        repository.save(new Contact(dados));
+    public ResponseEntity register(@RequestBody @Valid ContactDTO dados, UriComponentsBuilder uriComponentsBuilder) {
+        var contact = new Contact(dados);
+        repository.save(contact);
+        var uri = uriComponentsBuilder.path("/medicos/{id}").buildAndExpand(contact.getId()).toUri();
+
+        return ResponseEntity.created(uri).body(new ContactDTO(contact));
     }
 
     @GetMapping
-    public List<ContactDTO> findAll() {
-        return repository.findAll().stream().map(c -> new ContactDTO(c.getId(), c.getName(), c.getPhone(),
-                c.getEmail(), new AddressContactDTO(c.getAddress()), c.getBirth(), c.getNotes()))
-                .toList();
+    public ResponseEntity<List<ContactDTO>> findAll() {
+        var contactsList = repository.findAll().stream().map(c -> new ContactDTO(c)).toList();
+        return ResponseEntity.ok(contactsList);
     }
 
 
     @PutMapping()
     @Transactional
-    public void update(@RequestBody ContactDTO data) {
+    public ResponseEntity update(@RequestBody ContactDTO data) {
         var contact = repository.getReferenceById(data.id());
         contact.updateInformations(data);
+        return ResponseEntity.ok(new ContactDTO(contact));
     }
 
     @DeleteMapping("/{id}")
     @Transactional
-    public void delete(@PathVariable Long id) {
+    public ResponseEntity delete(@PathVariable Long id) {
         Optional<Contact> contact = repository.findById(id);
         if (contact.isPresent()) repository.deleteById(contact.get().getId());
+        return ResponseEntity.noContent().build();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<ContactDTO> findById(@PathVariable Long id) {
+        var contact = repository.findById(id);
+        if (contact.isPresent()) return ResponseEntity.ok(new ContactDTO(contact.get()));
+        return ResponseEntity.notFound().build();
     }
 
 }
--- src/main/java/br/com/lucasdev/contacts/dto/ContactDTO.java
+++ src/main/java/br/com/lucasdev/contacts/dto/ContactDTO.java
@@ -1,5 +1,6 @@
 package br.com.lucasdev.contacts.dto;
 
+import br.com.lucasdev.contacts.domain.contact.Contact;
 import jakarta.validation.constraints.NotBlank;
 
 import java.time.LocalDate;
@@ -15,4 +16,9 @@ public record ContactDTO(
         LocalDate birth,
         String notes
 ) {
+        public ContactDTO(Contact contact) {
+                this(contact.getId(), contact.getName(), contact.getPhone(),
+                        contact.getEmail(), new AddressContactDTO(contact.getAddress()),
+                        contact.getBirth(), contact.getNotes());
+        }
 }
